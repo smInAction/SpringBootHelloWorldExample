6e1a4ce4f91a95d3380e20013843c606409b4329
Shalu Malhotra <Shalu_Malhotra@epam.com>
2021-10-11 01:06:34 +0530

    turned to rest api

--- src/main/java/org/epam/helloworld/action/HelloWorldController.java
+++ src/main/java/org/epam/helloworld/action/HelloWorldController.java
@@ -1,17 +1,15 @@
 
 package org.epam.helloworld.action;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloWorldController {
  
- @RequestMapping("/helloworld")
- public ModelAndView hello() {
+ @GetMapping("/hello")
+ public String sayHello() {
  
-  String helloWorldMessage = "This is Hello world from Epam!";
-  return new ModelAndView("hello", "message", helloWorldMessage);
+  return "Hello world from Epam!";
  }
 }
